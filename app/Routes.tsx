fbc85e7ddd159327b53503c1c095b21b79b5e51f
ParkJongHyo <s18052@gsm.hs.kr>
2020-07-06 14:50:11 +0900

    Revert "Calculator_page_front"
    This reverts commit 10dad7ba4c3d49946a3cc3497b5b2ebe4cd561c8.

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -5,14 +5,12 @@ import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
 import NewPage from './containers/NewPage';
-import CalcPage from './containers/CalcPage';
 
 export default function Routes() {
   return (
     <App>
       <Switch>
         <Route path={routes.NEW} component={NewPage} />
-        <Route path={routes.CALCULATOR} component={CalcPage} />
         <Route path={routes.COUNTER} component={CounterPage} />
         <Route path={routes.HOME} component={HomePage} />
       </Switch>
--- app/containers/CalcPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Calculator from '../components/Calculator';
-
-export default function CalcPage() {
-  return <Calculator />;
-}
